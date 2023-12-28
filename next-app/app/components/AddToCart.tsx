cefde8f29d8b4ec6cb35aa7c0b23160f3bfd003a
Gautam Kumar <129857715+whoisgautxm@users.noreply.github.com>
2023-12-28 14:29:48 +0530

    Done
    this repository is Just the basics of Next.js . It is about the the 2 types of rendering and how file-routing works in Next.js

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+function AddToCart() {
+  return (
+    <div>
+      <button className='btn btn-active btn-secondary' onClick={()=> console.log('Click')}>Add to cart</button>
+    </div>
+  )
+}
+
+export default AddToCart
---
+++ next-app/app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewUserPage() {
+  return (
+    <div>
+      hii
+    </div>
+  )
+}
+
+export default NewUserPage
